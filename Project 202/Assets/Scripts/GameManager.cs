d1caba3ce130efd8f4edd5630248d321d2797d85
FortifiedOwO <FortifiedOwO@users.noreply.github.com>
2021-02-09 14:52:27 +0100

    Thijmens gedeelte WebGLBuild
    Chatbox plus design gedeelte van de webGL build voor woensdag

---
+++ Project 202/Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int maxMessages = 25;
+
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chatBox.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessage(chatBox.text, Message.MessageType.playerMessage);
+                chatBox.text = "";
+            }
+        }
+        else
+        {
+            if(!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+            {
+                chatBox.ActivateInputField();
+            }
+        }
+    }
+
+    public void SendMessage(string text, Message.MessageType messageType)
+    {
+        if (messageList.Count >= maxMessages)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatBox.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+
+        newMessage.textObject.text = newMessage.text;
+
+        messageList.Add(newMessage);
+    }
+
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textObject;
+    public MessageType messageType;
+
+    public enum MessageType
+    {
+        playerMessage,
+        info
+    }
+}
