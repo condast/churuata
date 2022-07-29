1b97954bc5d4d20d2e177e4f54401a1d3f058401
condast <info@condast.com>
2022-07-29 15:07:28 +0200

    Embedded comaker software

---
+++ Workspace/org.churuata.caminantes/src/org/churuata/caminantes/http/HomeServlet.java
@@ -0,0 +1,19 @@
+package org.churuata.caminantes.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final String S_HOME_PATH = "/caminantes/web/comaker/index.html";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect(req.getContextPath() + S_HOME_PATH);
+	}
+}
